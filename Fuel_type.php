0d0936eeb5e5fefaa6b56c54bf9b8b1d42d70eb9
mbaljak1 <92744348+mbaljak1@users.noreply.github.com>
2022-01-17 14:57:30 +0100

    Add files via upload

---
+++ Fuel_type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fuel_type extends Model
+{
+    protected $table = 'fuel_types';
+    use HasFactory;
+}
